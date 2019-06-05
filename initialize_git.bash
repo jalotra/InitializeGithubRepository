2048f212f3131ec3c88dbdc552d0e5a002ae680b
Shivam <jalotrashivam9@gmail.com>
2019-06-05 09:32:33 +0530

    changed shivam to

--- initialize_git.bash
+++ initialize_git.bash
@@ -9,7 +9,8 @@
 #and python3Project is the $2 variable
 
 function creatingLocalRepository(){
-    $USERNAME = ''                                       #ADD YOUR GITHUB USERNAME
+    GITHUBUSERNAME=""                                    #ADD YOUR GITHUB USERNAME
+    # echo $GITHUBUSERNAME
     currentPath=`pwd`
     # echo ${currentPath}
     # if [ $2 ];then
@@ -40,10 +41,10 @@ function creatingLocalRepository(){
     #Create a README.md
     touch README.md
     #Giving Name of the folder to README.md
-    echo "Hello World, My master who goes with the name $USERNAME just created me.
+    echo "Hello World, My master who goes with the USERNAME $GITHUBUSERNAME just created me.
     I hope that I you will find me helpful.I am happy to serve you " > README.md
     #Adding the Remote to this empty Git Repository
-    git remote add origin https://github.com/${USERNAME}/$1.git
+    git remote add origin https://github.com/$GITHUBUSERNAME/$1.git
     # Adding all the files present in the current repository
     #Adding SOME FOLDERS
     mkdir src docs 
